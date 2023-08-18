c7a238626e53a57d837cbb55239c7ce61a362ce6
Ewan Harrow <ewan@harrow.org>
2023-08-18 11:20:35 +0100

    test: empty POST payloads

--- web/test/integration/controller.test.js
+++ web/test/integration/controller.test.js
@@ -15,13 +15,31 @@ describe("Controller", () => {
 
     describe("post config", () => {
         it("should return 201 and the result", async () => {
+            const payload = {
+                                "current_animation": "spooky_rainbows",
+                                "spooky_rainbows": {
+                                    "hue_start": 30,
+                                    "hue_end": 140,
+                                    "speed": 0.3
+                                }
+                             }
+            const response = await request(app)
+                .post("/")
+                .set("content-type", "application/json")
+                .send(payload);
+
+            expect(response.status).toBe(201);
+            expect(response.body).toHaveProperty('success', true);
+        });
+
+        it("should return 400 and the failed result for empty payload", async () => {
             const response = await request(app)
                 .post("/")
                 .set("content-type", "application/json")
                 .send();
 
-            expect(response.status).toBe(201);
-            expect(response.body).toHaveProperty('success');
+            expect(response.status).toBe(400);
+            expect(response.body).toHaveProperty('success', false);
         });
     });
 });
