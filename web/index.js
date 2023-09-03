73b35bc3c1459d6379185c78ba0972331bc5cb6d
Ewan Harrow <ewan@harrow.org>
2023-09-03 16:47:49 +0100

    chore: dprint applied

--- web/index.js
+++ web/index.js
@@ -1,6 +1,6 @@
-const app = require('./server');
-const port = 3000
+const app = require("./server");
+const port = 3000;
 
 app.listen(port, () => {
-  console.log(`app listening on port ${port}`)
+  console.log(`app listening on port ${port}`);
 });
