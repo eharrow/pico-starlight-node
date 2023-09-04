5399a7743b3dbf7d20ad39956c19310053892aad
Ewan Harrow <ewan@harrow.org>
2023-09-04 18:14:00 +0100

    remove boilerplate react component

--- web/src/App.js
+++ web/src/App.js
@@ -1,33 +1,35 @@
 import React, { useState } from "react";
 
 import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
 import Toast from "react-bootstrap/Toast";
-
 import "./App.css";
 
-const ExampleToast = ({ children }) => {
-  const [show, toggleShow] = useState(true);
+const options = [{ id: 1, label: "one" }, { id: 2, label: "too" }, { id: 3, label: "three" }];
 
+async function getConfig() {
+  const resp = await fetch("http://192.168.178.2:4000");
+  const config = await resp.json();
+  console.log(config);
+}
+
+const SelectAnimation = () => {
+  const handleChange = event => {
+    console.log(event.target.value);
+  };
   return (
-    <Toast show={show} onClose={() => toggleShow(!show)}>
-      <Toast.Header>
-        <strong className="mr-auto">React-Bootstrap</strong>
-      </Toast.Header>
-      <Toast.Body>{children}</Toast.Body>
-    </Toast>
+    <Form.Select id="animation" aria-label="Animation selection" onChange={handleChange}>
+      <option>Choose animation</option>
+      {options.map(({ id, label }, index) => <option key={id} value={id}>{label}</option>)}
+    </Form.Select>
   );
 };
 
 const App = () => (
   <Container className="p-3">
-    <Container className="p-5 mb-4 bg-light rounded-3">
-      <h1 className="header">Welcome To React-Bootstrap</h1>
-      <ExampleToast>
-        We now have Toasts
-        <span role="img" aria-label="tada">
-          🎉
-        </span>
-      </ExampleToast>
+    <Container>
+      <h1>Starlight Settings</h1>
+      <SelectAnimation></SelectAnimation>
     </Container>
   </Container>
 );
