c58fae3330f9b37f0d57b4cc5b678033b58df7f6
Ewan Harrow <134166+eharrow@users.noreply.github.com>
2023-12-06 10:40:15 +0000

    Updates (#14)
    * chore: use dprint
    
    * refactor: bind express to all interfaces
    
    * feat: add req logging middleware

--- web/index.js
+++ web/index.js
@@ -1,6 +1,6 @@
-const app = require('./server');
-const port = 3000
+const app = require("./server");
+const port = 3000;
 
-app.listen(port, () => {
-  console.log(`app listening on port ${port}`)
+app.listen(port, "0.0.0.0", () => {
+  console.log(`app listening on port ${port}`);
 });
--- web/server.js
+++ web/server.js
@@ -1,59 +1,61 @@
-const path = require('node:path');
-const fs = require('fs');
-const express = require('express')
-const actuator = require('express-actuator')
+const path = require("node:path");
+const fs = require("fs");
+const express = require("express");
+const actuator = require("express-actuator");
 
-const app = express()
+const app = express();
 
-app.use(express.json()) // for parsing application/json
-app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use(actuator())
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(actuator());
 
-app.get('/', (req, res) => {
-    console.log('GET request to the homepage');
+app.use((req, res, next) => {
+  console.log(`${new Date().toISOString()} ${req.method} ${req.url} ${req.hostname}`);
+  next();
+});
 
-    var options = {
-        root: path.join(__dirname, 'www'),
-        dotfiles: 'deny',
-        headers: {
-          'x-timestamp': Date.now(),
-          'x-sent': true,
-          'Pragma': 'no-cache',
-          'Expires': 'Thu, 1 Jan 1970 00:00:00 GMT',
-          'Cache-Control': 'max-age=0, no-store, no-cache, must-revalidate'
-        }
-    };
-  res.sendFile('index.html', options);
-})
+app.get("/", (req, res) => {
+  var options = {
+    root: path.join(__dirname, "www"),
+    dotfiles: "deny",
+    headers: {
+      "x-timestamp": Date.now(),
+      "x-sent": true,
+      "Pragma": "no-cache",
+      "Expires": "Thu, 1 Jan 1970 00:00:00 GMT",
+      "Cache-Control": "max-age=0, no-store, no-cache, must-revalidate",
+    },
+  };
+  res.sendFile("index.html", options);
+});
 
-app.post('/', (req, res) => {
-    console.log('POST request to the homepage');
-    const json_req = JSON.stringify(req.body);
+app.post("/", (req, res) => {
+  const json_req = JSON.stringify(req.body);
 
-    if (json_req.trim().length === 0 || json_req == '{}') {
-      console.log("Error empty payload");
-      res.status(400).send({success:false});
-    }
+  if (json_req.trim().length === 0 || json_req == "{}") {
+    console.log("Error empty payload");
+    res.status(400).send({ success: false });
+  }
 
-    const retval = writeConfig(json_req);
-    if (retval) {
-      res.status(201).send({success:true});
-    } else {
-      res.status(500).send({success:false});
-    }
-})
+  const retval = writeConfig(json_req);
+  if (retval) {
+    res.status(201).send({ success: true });
+  } else {
+    res.status(500).send({ success: false });
+  }
+});
 
-app.use(express.static('www'));
+app.use(express.static("www"));
 
 function writeConfig(config) {
-    try {
-      fs.writeFileSync('www/config.json', config);
-      // file written successfully
-      return true;
-    } catch (err) {
-      console.error(err);
-      return false;
-    }
+  try {
+    fs.writeFileSync("www/config.json", config);
+    // file written successfully
+    return true;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
 }
 
 module.exports = app;
